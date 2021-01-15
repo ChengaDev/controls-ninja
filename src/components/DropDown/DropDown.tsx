5911a003b0554bc5382a4ab9f9d6bfb764ce863f
Chen Gazit <gazit.chen@gmail.com>
2021-01-15 10:23:59 +0200

    basic change of selceted option

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -4,32 +4,43 @@
 
 import React, { useState } from "react";
 
-import DropDownMenu from './DropDownMenu';
-import Option from '../../models/Option';
+import DropDownMenu from "./DropDownMenu";
+import Option from "../../models/Option";
 
-import '../../styles/DropDown.css';
+import "../../styles/DropDown.css";
 
-export type DropDownProps = { 
-    nonSelectionText: String, 
-    options: Option[], 
-    selectedOption?: Option, 
-    renderItem?: () => JSX.Element, 
-    onChange?: (e: any, option: Option) => void,
-    width?: String
+export type DropDownProps = {
+    nonSelectionText: String;
+    options: Option[];
+    selectedOption?: Option;
+    renderItem?: () => JSX.Element;
+    onChange?: (e: any, option: Option) => void;
+    width?: String;
 };
 
 const DropDown = (props: DropDownProps) => {
     const [showDropDown, setShowDropDown] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(null);
 
     const onChange = (event: any, option: Option) => {
         setShowDropDown(false);
+        setSelectedOption(option);
         props.onChange?.(event, option);
     };
 
-    return <>
-        <div className="nj-dropdown button rounded" onClick={() => setShowDropDown(!showDropDown)}>{props.nonSelectionText}</div>
-        {showDropDown && <DropDownMenu onChange={onChange} options={[{label: 'Option1', value: '1'}, {label: 'Option2', value: '2'}]} />}
-    </>
-}
+    return (
+        <>
+            <div
+                className="nj-dropdown button rounded"
+                onClick={() => setShowDropDown(!showDropDown)}
+            >
+                {selectedOption ? selectedOption.label : props.nonSelectionText}
+            </div>
+            {showDropDown && (
+                <DropDownMenu onChange={onChange} options={props.options} />
+            )}
+        </>
+    );
+};
 
 export default DropDown;
--- src/stories/DropDown.stories.tsx
+++ src/stories/DropDown.stories.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-import DropDown, { DropDownProps } from '../components/DropDown/DropDown';
+import DropDown, { DropDownProps } from "../components/DropDown/DropDown";
+
+const options = [
+    { label: "Option1", value: "1" },
+    { label: "Option2", value: "2" }
+];
 
 export default {
-    title: 'Inputs/DropDown',
+    title: "Inputs/DropDown",
     component: DropDown
 } as Meta;
 
-const DropDownRender: Story<DropDownProps> = (args) => <DropDown {...args} />;
+const DropDownStory: Story<DropDownProps> = (args) => <DropDown {...args} />;
 
-export const HelloWorld = DropDownRender.bind({});
-HelloWorld.args = {
-    nonSelectionText: 'Select an option',
-    onChange: (e, option) => { alert(option.label) }
+export const BasicRender = DropDownStory.bind({});
+BasicRender.args = {
+    nonSelectionText: "Select an option",
+    options,
+    onChange: (e, option) => {
+        alert(option.label);
+    }
 };
